07e1396162cedad1ff719b06d5a95088459f63f6
vkdev <dev.victorkaua@gmail.com>
2026-04-27 13:14:06 -0300

    conexão com mysql

--- src/db/DB.java
+++ src/db/DB.java
@@ -1,5 +1,51 @@
 package db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DB {
+    //METODO PARA CONEXÃO COM BANCO DE DADOS
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Properties props = loadProperties();
+                String url = props.getProperty("dburl");
+                conn = DriverManager.getConnection(url, props);
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    // METODO PARA FECHAR O BANCO DE DADOS
+
+    public static void closeConnection() {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    // METOOO PARA LEITURA DO ARQUIVO DB
+    private static Properties loadProperties() {
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        } catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
 
 }
